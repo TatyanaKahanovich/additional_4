311b47c1f53fffe657697a5765a364d4768c9366
Tatyana Kahanovich <d-m-tanya@yandex.ru>
2017-09-24 14:26:10 +0300

    result of Product task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
-module.exports = function multiply(first, second) {
-  // your solution
+module.exports = function multiply(first, second){
+	var resultMultiply = [],
+        first = first.split('').reverse(),
+        second = second.split('').reverse(),
+	    lenFirst = first.length,
+	    lenSecond = second.length;
+
+
+    for (var i = 0; i < lenFirst; i++){
+        for (var j = 0; j < lenSecond; j++){
+            if (!resultMultiply[i + j]){
+                resultMultiply[i + j] = 0;
+            }
+            resultMultiply[i + j] += first[i] * second[j];
+        }
+    }
+	var lenRes = resultMultiply.length;
+    for (var i = 0; i < lenRes; i++){
+        if (resultMultiply[i] >= 10){
+            if (!resultMultiply[i + 1]){
+                resultMultiply[i + 1] = 0;
+            }
+            resultMultiply[i + 1] += parseInt(resultMultiply[i] / 10);
+            resultMultiply[i] %= 10;
+        }
+    }
+
+    return resultMultiply.reverse().join('');
 }
